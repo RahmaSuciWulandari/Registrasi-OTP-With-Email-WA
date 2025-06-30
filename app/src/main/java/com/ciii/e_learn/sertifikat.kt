dc4320847333274f32bbe3e4b1c7fc7c1910c2c6
Suci <rahmawul504@gmail.com>
2025-06-30 09:15:00 +0700

    Update

--- app/src/main/java/com/ciii/e_learn/sertifikat.kt
+++ app/src/main/java/com/ciii/e_learn/sertifikat.kt
@@ -24,7 +24,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.ciii.e_learn.R // Pastikan import R dari projectmu
+
 
 @Composable
 fun CertificateScreen(navController: NavController) {
